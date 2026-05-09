b5b8006f80d50388de2d012bf8fcc04b3ccb3eac
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2026-05-09 16:20:39 +0000

    Added Aura

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const gojoGif = document.getElementById('gojoGif');
+const gojoButton = document.getElementById('gojoButtOn');
+
+gojoButton.addEventListener('click', () => {
+    // Increase image size
+    document.getElementById('BODY').style.backgroundColor = 'purple';
+});
